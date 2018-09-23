919ca235388303cfe9530d15de6adb56ddc365f5
liyan <yanx0167@stud.kea.dk>
2018-09-23 14:19:30 +0200

    changed: color of bg, menu button and aside

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const menu = document.querySelector(".burger");
+const menu = document.querySelector(".burger p");
 const aside = document.querySelector("aside");
 
 window.addEventListener("DOMContentLoaded", init);
@@ -8,4 +8,5 @@ function init() {
 }
 function toggleAside() {
   aside.classList.toggle("show");
+  menu.classList.toggle("show");
 }
