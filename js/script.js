a34916ce3eff8ff3b0beee4725708f789d02c6b5
liyan <yanx0167@stud.kea.dk>
2018-10-06 11:02:32 +0200

    added: modal in sync with scroll

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function listenNav(n) {
   function navClicked(m) {
     navSpans.forEach(n => n.classList.add("uncheck"));
     m.target.classList.remove("uncheck");
+    closeModal();
     let target = m.target.dataset.section;
     document.querySelector(`#${target}`).scrollIntoView({
       block: "center",
@@ -24,10 +25,7 @@ function listenProject(p) {
   p.addEventListener("click", showSingleProject);
   function showSingleProject(m) {
     // reset
-    modal.style.width = "0";
-    modal.style.height = "0";
-    modal.style.left = "0";
-    modal.style.left = "0";
+    closeModal();
     // update
     let positionX = m.target.getBoundingClientRect().left;
     let positionY = m.target.getBoundingClientRect().top;
@@ -39,19 +37,46 @@ function listenProject(p) {
     function enlargeModal() {
       let width = modal.getBoundingClientRect().width;
       if (
-        (positionX >= viewportWidth / 2 && width < positionX * 1) ||
+        (positionX >= viewportWidth / 2 && width < positionX * 0.5) ||
         (positionX < viewportWidth / 2 &&
-          width < (viewportWidth - positionX) * 1)
+          width < (viewportWidth - positionX) * 0.5)
       ) {
-        // modal should cover whole screen, when the clicked element is in the right half of the screen, make sure modal extend to the left edge of the screen
-        // modal should cover whole screen, when the clicked element is in the left half of the screen, the needed width extension is the window width - positionX
         width += (viewportWidth / 1000) * 60;
         modal.style.width = `${width}px`;
         modal.style.height = `${width}px`; // for the modal div, height and width are the same
         modal.style.left = `${positionX - width / 2}px`;
         modal.style.top = `${positionY - width / 2}px`;
         setTimeout(enlargeModal, 1000 / 60);
+      } else {
+        let positionYInit = m.target.getBoundingClientRect().top;
+        let scrollTopInit =
+          window.pageYOffset ||
+          (
+            document.documentElement ||
+            document.body.parentNode ||
+            document.body
+          ).scrollTop;
+        window.addEventListener("scroll", syncScroll);
+        function syncScroll() {
+          let scrollTop =
+            window.pageYOffset ||
+            (
+              document.documentElement ||
+              document.body.parentNode ||
+              document.body
+            ).scrollTop; // !!! remember !!!
+          modal.style.top = `${positionYInit -
+            width / 2 -
+            scrollTop +
+            scrollTopInit}px`;
+        }
       }
     }
   }
+}
+function closeModal() {
+  modal.style.width = "0";
+  modal.style.height = "0";
+  modal.style.left = "0";
+  modal.style.left = "0";
 }
