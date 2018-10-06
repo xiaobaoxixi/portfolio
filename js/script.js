30fd9ab5d47477fe4506439ea2246916f262f0ad
liyan <yanx0167@stud.kea.dk>
2018-10-06 08:45:44 +0200

    added: modal box, no dynamic position yet

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
 "use strict";
-// const menu = document.querySelector(".burger p");
-// const aside = document.querySelector("aside");
-
-// window.addEventListener("DOMContentLoaded", init);
-// function init() {
-//   menu.addEventListener("click", toggleAside);
-// }
-// function toggleAside() {
-//   aside.classList.toggle("show");
-//   menu.classList.toggle("show");
-// }
 const navSpans = document.querySelectorAll("nav span");
-navSpans.forEach(listen);
-function listen(n) {
+const projects = document.querySelectorAll(".project-group li span");
+const modal = document.querySelector("#single-project");
+
+// navigation
+navSpans.forEach(listenNav);
+function listenNav(n) {
   n.addEventListener("click", navClicked);
   function navClicked(m) {
     navSpans.forEach(n => n.classList.add("uncheck"));
     m.target.classList.remove("uncheck");
+    modal.classList.remove("show");
     let target = m.target.dataset.section;
-    console.log(target);
     document.querySelector(`#${target}`).scrollIntoView({
       block: "center",
       inline: "nearest",
       behavior: "smooth"
     });
+  }
+}
+// display single project
+projects.forEach(listenProject);
+function listenProject(p) {
+  p.addEventListener("click", showSingleProject);
+  function showSingleProject(m) {
+    console.log(m.target);
+    modal.classList.add("show");
   }
 }
